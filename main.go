ff09d83e51b8f46f16bf0a182134d5ad0760f9c7
padznich <slabkopg@gmail.com>
2017-03-30 13:55:13 +0300

    [ADD] Task 1

--- main.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func main() {
-	println("it's over 9000!")
-}
---
+++ task_1/main_test.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+	"strings"
+	"strconv"
+	"sort"
+)
+
+// HomeWork Functions
+func ReturnInt() int {
+	return 1
+}
+
+func ReturnFloat() float32 {
+	return 1.1
+}
+
+func ReturnIntArray() [3]int {
+	a := [...]int{1, 3, 4}
+	return a
+}
+
+func ReturnIntSlice() []int {
+	a := []int{1, 2, 3}
+	return a
+}
+
+func IntSliceToString(sl []int) string {
+
+	sl_str := make([]string, len(sl))
+	for i, v := range sl {
+		sl_str[i] = strconv.Itoa(v)
+	}
+	out := strings.Join(sl_str, "")
+
+	return out
+}
+
+func MergeSlices(sl_f []float32, sl_i []int32) []int {
+
+	sl_fi := make([]int32, len(sl_f))
+	for i, v := range sl_f {
+		sl_fi[i] = int32(v)
+	}
+	tmp := append(sl_fi, sl_i...)
+	out := []int{}
+
+	for _, i := range tmp {
+		out = append(out, int(i))
+	}
+
+	return out
+}
+
+func GetMapValuesSortedByKey(inp map[int]string) []string {
+
+	out := []string{}
+	keys := []int{}
+
+	for k := range inp {
+		keys = append(keys, k)
+	}
+
+	sort.Ints(keys)
+
+	for _, key := range keys {
+		out = append(out, inp[key])
+	}
+
+	return out
+}
+
+
+// Task
+func TestReturnInt(t *testing.T) {
+	if ReturnInt() != 1 {
+		t.Error("expected 1")
+	}
+}
+
+func TestReturnFloat(t *testing.T) {
+	if ReturnFloat() != float32(1.1) {
+		t.Error("expected 1.1")
+	}
+}
+
+func TestReturnIntArray(t *testing.T) {
+	if ReturnIntArray() != [3]int{1, 3, 4} {
+		t.Error("expected '[3]int{1, 3, 4}'")
+	}
+}
+
+func TestReturnIntSlice(t *testing.T) {
+	expected := []int{1, 2, 3}
+	result := ReturnIntSlice()
+	if !reflect.DeepEqual(result, expected) {
+		t.Error("expected", expected, "have", result)
+	}
+}
+
+func TestIntSliceToString(t *testing.T) {
+	expected := "1723100500"
+	result := IntSliceToString([]int{17, 23, 100500})
+	if expected != result {
+		t.Error("expected", expected, "have", result)
+	}
+}
+
+func TestMergeSlices(t *testing.T) {
+	expected := []int{1, 2, 3, 4, 5}
+	result := MergeSlices([]float32{1.1, 2.1, 3.1}, []int32{4, 5})
+	if !reflect.DeepEqual(result, expected) {
+		t.Error("expected", expected, "have", result)
+	}
+}
+
+func TestGetMapValuesSortedByKey(t *testing.T) {
+
+	var cases = []struct {
+		expected []string
+		input    map[int]string
+	}{
+		{
+			expected: []string{
+				"Январь",
+				"Февраль",
+				"Март",
+				"Апрель",
+				"Май",
+				"Июнь",
+				"Июль",
+				"Август",
+				"Сентябрь",
+				"Октябрь",
+				"Ноябрь",
+				"Декарь",
+			},
+			input: map[int]string{
+				9:  "Сентябрь",
+				1:  "Январь",
+				2:  "Февраль",
+				10: "Октябрь",
+				5:  "Май",
+				7:  "Июль",
+				8:  "Август",
+				12: "Декарь",
+				3:  "Март",
+				6:  "Июнь",
+				4:  "Апрель",
+				11: "Ноябрь",
+			},
+		},
+
+		{
+			expected: []string{
+				"Зима",
+				"Весна",
+				"Лето",
+				"Осень",
+			},
+			input: map[int]string{
+				10: "Зима",
+				30: "Лето",
+				20: "Весна",
+				40: "Осень",
+			},
+		},
+	}
+
+	for _, item := range cases {
+		result := GetMapValuesSortedByKey(item.input)
+		if !reflect.DeepEqual(result, item.expected) {
+			t.Error("expected", item.expected, "have", result)
+		}
+	}
+}
